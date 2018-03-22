a173c35c77c443d5101046e937b33e5d256588e6
kurtnovack <kurt@walltowall.com>
2018-03-21 22:10:50 -0400

    adds terminal assets and sets up level selection

---
+++ Assets/Hacker.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker :MonoBehaviour {
+
+	// Game state
+	int level;
+	enum Screen { MainMenu, Password, Win };
+	Screen currentScreen;
+
+	// Use this for initialization
+	void Start () {
+		ShowMainMenu();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnUserInput(string input) {
+		if (input == "menu") {
+			ShowMainMenu();
+		} else if (currentScreen == Screen.MainMenu) {
+			RunMainMenu(input);
+		} else if (currentScreen == Screen.Password) {
+
+		} else if (currentScreen == Screen.Win) {
+
+		}
+	}
+
+	void ShowMainMenu() {
+		currentScreen = Screen.MainMenu;
+
+		Terminal.ClearScreen();
+		Terminal.WriteLine("What would you like to hack into?\n\n" +
+			"Press 1 for the high school\n" +
+			"Press 2 for the police department\n" +
+			"Press 3 for the government\n\n" +
+			"Enter your selection: "
+		);
+	}
+
+	void RunMainMenu(string input) {
+		if (input == "1") {
+			level = 1;
+			StartGame();
+		} else if (input == "2") {
+			level = 2;
+			StartGame();
+		} else if (input == "3") {
+			level = 3;
+			StartGame();
+		} else if (input == "007") {
+			Terminal.WriteLine("Please choose a level, Mr. Bond.");
+		} else {
+			Terminal.WriteLine("Please choose a valid level.");
+		}
+	}
+
+	void StartGame() {
+		currentScreen = Screen.Password;
+		Terminal.WriteLine("You have chosen level " + level);
+		Terminal.WriteLine("Please enter your password: ");
+	}
+}
